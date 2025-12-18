85cc011531772eba5b940d51ee4f1bd809cf2851
Kyle Ross <kyle_ross@evt.com>
2025-12-18 16:59:50 +1100

    Generalised connection function for snowflake named connections - query_result_via_snowflake_connection()

---
+++ evtscripts/query_utils/query_snowflake.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+import pandas as pd
+from pandas import DataFrame
+from snowflake.connector import connect, SnowflakeConnection  # type: ignore
+from snowflake.connector.cursor import SnowflakeCursor
+
+
+def query_result_via_snowflake_connection(connection_name: str, sql: str) -> DataFrame:
+    # Gets connection via connection name
+    conn: SnowflakeConnection = connect(connection_name = connection_name)
+
+    # Initialise empty dataframe
+    df: Optional[pd.DataFrame] = None
+
+    # Define the query to be run
+    sql = "SELECT * FROM EDW_ENT_PRD.CURATED.DIM_VH_CIN"
+
+    # Temporarily init the cursor and execute the sql
+    try:
+        cursor: SnowflakeCursor = conn.cursor()
+        cursor.execute(sql)
+
+        # Snowflake returns a pandas DataFrame here
+        df = cursor.fetch_pandas_all()
+    finally:
+        conn.close()
+
+    # Return the result
+    return df
