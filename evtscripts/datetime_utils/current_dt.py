6e932d898e46302f00162acc2a5a342103d347c4
Kyle Ross <kyle_ross@evt.com>
2025-12-18 17:18:21 +1100

    Changed format string

--- evtscripts/datetime_utils/current_dt.py
+++ evtscripts/datetime_utils/current_dt.py
@@ -2,5 +2,5 @@ from datetime import datetime
 from typing import Final
 
 def current_datetime_str() -> str:
-    FORMAT: Final[str] = "%Y-%m-%d %H:%M:%S"
+    FORMAT: Final[str] = "%Y-%m-%d %H-%M-%S"
     return datetime.now().strftime(FORMAT)
