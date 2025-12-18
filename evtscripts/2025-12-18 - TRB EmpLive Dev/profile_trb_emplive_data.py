bcbde1cadb3517f07939681b412247e6c7929615
Kyle Ross <kyle_ross@evt.com>
2025-12-18 17:27:42 +1100

    Finalise query that generates the data profile

--- evtscripts/2025-12-18 - TRB EmpLive Dev/profile_trb_emplive_data.py
+++ evtscripts/2025-12-18 - TRB EmpLive Dev/profile_trb_emplive_data.py
@@ -1,11 +1,20 @@
-from evtscripts.query_utils.query_snowflake import query_result_via_snowflake_connection
+from ydata_profiling import ProfileReport  # type: ignore
 
+from evtscripts.query_utils.query_snowflake import query_result_via_snowflake_connection
+from evtscripts.datetime_utils.current_dt import current_datetime_str
+
+# Define constants
 connection_name = "mm31132.ap-southeast-2"
 query = """
     SELECT * 
     FROM EDW_ENT_PRD.CURATED.DIM_VH_CIN
 """
+output_name = "thredbo_emplive_profile_report"
+relative_output_path = "evtscripts/2025-12-18 - TRB EmpLive Dev/output/"
 
+# Get data
 df = query_result_via_snowflake_connection(connection_name, query)
 
-print(df)
+# Generate profile
+profile = ProfileReport(df, title=output_name)
+profile.to_file(f"{relative_output_path}{output_name} {current_datetime_str()}.html")
