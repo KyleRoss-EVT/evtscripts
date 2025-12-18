0854dbcb0516d6be46c7eafe880a766b1eceb0e2
Kyle Ross <kyle_ross@evt.com>
2025-12-18 17:14:43 +1100

    Added datetime utils

--- evtscripts/__init__.py
+++ evtscripts/__init__.py
@@ -1,5 +1,7 @@
 import evtscripts.query_utils as query_utils
+import evtscripts.datetime_utils as datetime_utils
 
 __all__ = [
-    "query_utils"
+    "query_utils",
+    "datetime_utils"
 ]
---
+++ evtscripts/datetime_utils/current_dt.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+from typing import Final
+
+def current_datetime_str() -> str:
+    FORMAT: Final[str] = "%Y-%m-%d %H:%M:%S"
+    return datetime.now().strftime(FORMAT)
