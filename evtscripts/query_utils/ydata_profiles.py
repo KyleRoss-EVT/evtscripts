8d6e4912468f0bd4ee570f9a0ada0c590ae651b5
Kyle Ross <kyle_ross@evt.com>
2026-01-23 14:23:28 +1100

    Refactor for pathcontext dataclass, plus regenerate

--- evtscripts/query_utils/ydata_profiles.py
+++ evtscripts/query_utils/ydata_profiles.py
@@ -5,36 +5,25 @@ from ydata_profiling import ProfileReport  # type: ignore
 from evtscripts.query_utils.query_snowflake import (
     query_result_via_snowflake_connection,
 )
-from evtscripts.datetime_utils.current_dt import current_datetime_str
+from evtscripts.query_utils.build_output_paths import PathContext
 
 
-def minimal_profile(
-    output_folder: str, file_name: str, query: str, dt_in_name: bool = True
-) -> None:
+def minimal_profile(path_context: PathContext, query: str) -> None:
     connection_name = "mm31132.ap-southeast-2"
-
-    # Build path with checks
-    folder_path = Path(output_folder)
-    if not folder_path.exists():
-        raise TypeError(f"output_folder path: '{output_folder}' does not exist")
-    if not folder_path.is_dir():
-        raise TypeError(f"output_folder path: '{output_folder}' is not a directory")
-    output_file_path = (
-        folder_path
-        / f"{file_name} {' ' + current_datetime_str() if dt_in_name else ''}.html"
-    )
 
     # Get data
     df = query_result_via_snowflake_connection(connection_name, query)
 
     # Generate profile
-    profile = ProfileReport(df, title=file_name, minimal=True)
-    profile.to_file(output_file_path)
+    profile = ProfileReport(df, title=path_context.file_name, minimal=True)
+    profile.to_file(path_context.output_file_path)
 
 
 if __name__ == "__main__":
-    minimal_profile(
-        "evtscripts/query_utils/example",
-        "minimal_data_profile_main",
-        "SELECT * FROM EDW_ENT_PRD.CURATED.DIM_VH_SITE",
+    path_context = PathContext(
+        Path("evtscripts/query_utils/example"),
+        "minimal_data_profile_main DIM_VH_SITE",
+        ".html",
+        dt_in_name=False,
     )
+    minimal_profile(path_context, "SELECT * FROM EDW_ENT_PRD.CURATED.DIM_VH_SITE")
